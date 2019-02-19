02b97111ca867c091830d3d7d1a5d4448950d5c3
LazareC <lazarela17@gmail.com>
2019-02-19 02:16:59 +0100

    mise en forme

--- search.py
+++ search.py
@@ -13,21 +13,26 @@ for name in files:
             lignes = fichier.read().splitlines()
             fichier.close()
 
-            search = "agateargent@gmail.com"
+            if len(sys.argv) != 1:
+                search = sys.argv[1]
+            else:
+                search = "mail@mail.fr" #if you do not use the arguments enter the mail here
+
 
             def recherche(search):
                 i = 0
                 for ligne in lignes:
                     mail = ligne.split(":")
                     if search in mail[0]:
-                        print(" \033[92m FIND: " + ligne + "\n Trouve dans: " + name)
-                        print("nombre de temtative: " + str(i))
+                        print("\033[92mFIND: " + ligne + "\nFound in: " + name)
+                        print("number of attempted: " + str(i))
                         exit(0)
                         break
                     else:
-                        print("\033[91m FAILD: " + ligne)
+                        print("\033[91mFAILD: " + ligne)
                         i += 1
-                print("nombre de temtative: " + str(i))
+                print("\033[91m" + search + " was not found")
+                print("number of attempted: " + str(i))
 
             recherche(search)
 
